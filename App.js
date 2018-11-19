3d41ef605e042c600e1f747d45d6f91ab8cde1df
Galamon Mac <galamon5@hotmail.com>
2018-11-19 14:54:28 -0600

    Eslint configuration

--- App.js
+++ App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nombre: 'SocialInsta',
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
